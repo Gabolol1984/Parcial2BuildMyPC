00613542786b63af97fddc5a5ba319d33d1315c7
VA-Alumno <kevi.abarca@duocuc.cl>
2026-05-12 12:13:50 -0400

    se agrega component

---
+++ msvc-component/src/main/java/msvc_component/msvc_component/MsvcComponentApplication.java
@@ -0,0 +1,13 @@
+package msvc_component.msvc_component;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcComponentApplication.class, args);
+	}
+
+}
---
+++ msvc-component/src/test/java/msvc_component/msvc_component/MsvcComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package msvc_component.msvc_component;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
