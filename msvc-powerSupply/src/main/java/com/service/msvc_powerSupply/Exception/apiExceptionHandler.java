1ccdc7bdd7fc8b0975ba44835ac031b6df0dd669
kevin <kevi.abarca@duocuc.cl>
2026-05-19 11:55:57 -0400

    se realiza apiExceptionHandler de psu

--- msvc-powerSupply/src/main/java/com/service/msvc_powerSupply/Exception/apiExceptionHandler.java
+++ msvc-powerSupply/src/main/java/com/service/msvc_powerSupply/Exception/apiExceptionHandler.java
@@ -1,4 +1,30 @@
 package com.service.msvc_powerSupply.Exception;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class apiExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError e : ex.getBindingResult().getFieldErrors()) {
+            errors.put(e.getField(), e.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(psuException.class)
+    public ResponseEntity<?> handlepsuException(psuException ex){
+        Map<String,String> error= new HashMap<>();
+        error.put("error","Ocurrio un problema al procesar la PSU. Detalle: "+ ex.getMessage());
+        return ResponseEntity.badRequest().body(error);
+    }
 }
