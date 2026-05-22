7eba18f42c0d7c1a28c80375e12fee2fc04bd9bf
Kevin <kevi.abarca@duocuc.cl>
2026-05-22 13:22:20 -0400

    se realiza msvc-build

--- msvc-component/src/main/java/msvc_component/msvc_component/MsvcComponentApplication.java
+++ msvc-component/src/main/java/msvc_component/msvc_component/MsvcComponentApplication.java
@@ -2,7 +2,9 @@ package msvc_component.msvc_component;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MsvcComponentApplication {
 
--- msvc-ram/src/main/java/msvc/msvc_ram/MsvcRamApplication.java
+++ msvc-ram/src/main/java/msvc/msvc_ram/MsvcRamApplication.java
@@ -2,7 +2,9 @@ package msvc.msvc_ram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MsvcRamApplication {
 
