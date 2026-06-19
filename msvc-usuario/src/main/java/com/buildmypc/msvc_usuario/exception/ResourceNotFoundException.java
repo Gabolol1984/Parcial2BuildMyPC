7b028cb9abf5d9b65c0b4687c654d00dac1db195
Kevin <kevi.abarca@duocuc.cl>
2026-06-18 20:31:12 -0400

    se elimina microservicio usuario para rehacerlo

--- msvc-usuario/src/main/java/com/buildmypc/msvc_usuario/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.buildmypc.msvc_usuario.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- msvc-usuario/src/main/java/com/buildmypc/msvc_usuario/exception/ServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.buildmypc.msvc_usuario.exception;
-
-public class ServiceException extends RuntimeException {
-    public ServiceException(String message) {
-        super(message);
-    }
-}
